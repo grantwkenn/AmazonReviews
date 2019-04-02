5a939a150b89683d8219972e739690144302ce7a
Richard Robinson <rrobins1@mail.sfsu.edu>
2019-04-01 18:53:41 -0700

    star sorting works

--- star_sorter.py
+++ star_sorter.py
@@ -1,5 +1,6 @@
 import csv
-
+import numpy
+import pandas
 
 videoPath = 'amazon_reviews_Video_Games.csv'
 kitchenPath = 'amazon_reviews_kitchen.csv'
@@ -15,17 +16,17 @@ four_star_path = base_path + label + '_four_star.csv'
 five_star_path = base_path + label + '_five_star.csv'
 
 
-file_one = open(one_star_path, 'w')
-file_two = open(two_star_path, 'w')
-file_three = open(three_star_path, 'w')
-file_four = open(four_star_path, 'w')
-file_five = open(five_star_path, 'w')
+file_one = open(one_star_path, 'w', encoding="utf8")
+file_two = open(two_star_path, 'w', encoding="utf8 ")
+file_three = open(three_star_path, 'w', encoding="utf8 ")
+file_four = open(four_star_path, 'w', encoding="utf8")
+file_five = open(five_star_path, 'w', encoding="utf8")
 
-writer_one = csv.writer(file_one) #quotechar="", quoting=csv.QUOTE_NONE)
-writer_two = csv.writer(file_two)# quotechar="", quoting=csv.QUOTE_NONE)
-writer_three = csv.writer(file_three)# quotechar="", quoting=csv.QUOTE_NONE)
-writer_four = csv.writer(file_four)# quotechar="", quoting=csv.QUOTE_NONE)
-writer_five = csv.writer(file_five)# quotechar="", quoting=csv.QUOTE_NONE)
+writer_one = csv.writer(file_one, quotechar="", quoting=csv.QUOTE_NONE)
+writer_two = csv.writer(file_two, quotechar="", quoting=csv.QUOTE_NONE)
+writer_three = csv.writer(file_three, quotechar="", quoting=csv.QUOTE_NONE)
+writer_four = csv.writer(file_four, quotechar="", quoting=csv.QUOTE_NONE)
+writer_five = csv.writer(file_five, quotechar="", quoting=csv.QUOTE_NONE)
 
 def rowToString (row):
     output = ""
@@ -35,51 +36,33 @@ def rowToString (row):
     output += '\n'
     return output
 
-
-with open(base_path + videoPath) as csv_file:
+with open(base_path + videoPath, encoding='utf8') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
             row = rowToString(row)
             file_one.write(row)
             file_two.write(row)
             file_three.write(row)
             file_four.write(row)
             file_five.write(row)
-            #writer_one.writerow(row)
-            #writer_two.writerow(row)
-            #writer_three.writerow(row)
-            #writer_four.writerow(row)
-            #writer_five.writerow(row)
             line_count = 1
 
         else:
             score = row[7]
             row = rowToString(row)
-            print(row)
             if score == str(1):
-                #writer_one.writerow(row)
                 file_one.write(row)
 
             elif score == str(2):
-                #writer_two.writerow(row)
                 file_two.write(row)
 
             elif score == str(3):
-                #writer_three.writerow(row)
                 file_three.write(row)
-            
+                
             elif score == str(4):
-                #writer_four.writerow(row)
                 file_four.write(row)
 
             elif score == str(5):
-                #writer_five.writerow(row)
                 file_five.write(row)
-
-print('finished')
-
-
-
