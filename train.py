460759c8ecc656ddb745841955d8814c80d1fdae
Richard Robinson <rrobins1@mail.sfsu.edu>
2019-04-22 10:37:07 -0700

    added some stuff
    Can  do body + header,  I think I did parts of it incorrectly. Will refactor.

--- train.py
+++ train.py
@@ -3,7 +3,9 @@ import pandas as pd
 import string
 import matplotlib.pyplot
 import scikitplot
-
+import random
+import time
+import numpy
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.svm import SVC
@@ -15,13 +17,19 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.naive_bayes import MultinomialNB
-
 from sklearn.model_selection import cross_val_predict
-
-#TODO helpful_votes, total_votes, vine, verified_purchase MAY BE USED AS WEIGHTS
-#TODO case, stem, stopwords
+#from sklearn.pipeline import FeatureUnion
+#from sklearn.pipeline import PCA, truncatedSVD
+#TODO IN PROGRESS  helpful_votes, total_votes, vine, verified_purchase 
+#TODO IN PROGRESS case, stem, stopwords --> GRANT
 #TODO guessed star vs actual difference
 #TODO more test files
+#TODO IN PROGRESS cross validation -> grant
+#TODO more features, nouns, adjectives 
+#TODO error analysis, highest magnitude
+#TODO output distrubution for tsting
+#TODO precision recall, f score --> 
+#TODO k-fold --> GRANT
 
 ###############################################################
 # BEGIN CONFIGURATION SETUP 
@@ -36,8 +44,7 @@ def findClassifier (classifier_name):
 #possible classifiers
 classifiers = [
     ("nu_svc" , NuSVC(gamma = 'auto')),
-    ("svc" , SVC(gamma = 'auto') ),
-    ("svc2" , SVC(kernel="linear", C=0.025)),
+    ("svc2" , SVC(kernel="linear", C=.5)),
     ("decision_tree" , DecisionTreeClassifier(max_depth=5)),
     ("random_forest" , RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1)),
     ("mlp" , MLPClassifier(alpha=1)),
@@ -51,8 +58,11 @@ kitchen_data = "Amazon Review Datasets/kitchen_truncated.csv"
 #possible features
 features = [
     "review_headline",
-    "review_body"
-    #"review_combined" #TODO combine these
+    "review_body",
+    "review_headline_body",
+    "helpful_votes",
+    "total_votes",
+    "vine"
 ]
 
 #possible types of classification
@@ -64,19 +74,27 @@ catagory_types = [
 #possible labels
 star_label = "star_rating"
 
-#features being selected
-testing_features = ["review_body"]
+
+##########################################################################
+#SYNTAX FOR FEATURE SELECTION
+#To select a combination of features { "feature_one", "feautre_two"}
+#To select multiple runs of features ["feature_one", "feature_two"]
+#These can be combined
+#######################################################################
+
+
+testing_features = ["review_headline", "review_body"]
 testing_labels = star_label
 selected_dataset = video_game_data
-selected_classifiers = ["nb", "svc2"]
+selected_classifiers = ["nb"]
 testing_types = ["boolean"]
 
-plot_confusion_matrix = True
+plot_confusion_matrix = False
 
 #use all options
-use_all_features = True
-use_all_classifiers = True
-use_all_types = True
+use_all_features = False
+use_all_classifiers = False
+use_all_types = False
 
 ############################################################
 # END CONFIGURATION SET UP
@@ -102,9 +120,11 @@ if use_all_types:
 #Generate Dataset, Undersample, Test
 ##########################################################
 
-def testDataset(classifier, testing_feature, data_type):
+def testDataset(classifier, testing_features, data_type):
     classifier_name = classifier[0]
     classifier = classifier[1]
+    
+    testing_features = ("review_body", "review_headline")
     
     if ( data_type == "boolean" ):
         binary_classification = True
@@ -113,41 +133,53 @@ def testDataset(classifier, testing_feature, data_type):
 
     #input data from CSV file
     input_data = pd.read_csv(selected_dataset)
+        
+    input_data['review_headline_body'] = input_data.review_headline + ' ' + input_data.review_body
 
     #define dataset
-    dataset = {"review_body": input_data["review_body"], "star_rating": input_data["star_rating"], "review_headline": input_data["review_headline"] }
+    dataset = {"review_body": input_data["review_body"].values, 
+        "star_rating": input_data["star_rating"].values, 
+        "review_headline": input_data["review_headline"].values,
+        "vine": str(input_data["vine"].values),
+        "helpful_votes": str(input_data["helpful_votes"].values),
+        "total_votes": str(input_data["total_votes"].values),
+        "review_headline_body" : input_data["review_headline_body"].values }
     dataset = pd.DataFrame(data = dataset)
     dataset = dataset.dropna() #drop missing values
 
+    
     if binary_classification: # omit 3 star reviews, binary classification
         dataset["label"] = dataset[star_label].apply(lambda rating : +1 if str(rating) > '3' else -1)
     else:
         dataset["label"] = dataset[star_label] # star classification
 
-    X = pd.DataFrame(dataset, columns = [testing_feature])
+    X = pd.DataFrame(dataset, columns = [testing_features])
     y = pd.DataFrame(dataset, columns = ["label"])
 
-    rus = RandomUnderSampler(random_state=13)
+    rus = RandomUnderSampler(random_state=random.seed(time.time()))
     X_res, y_res = rus.fit_resample(X, y)
 
 
-    X1 = pd.DataFrame(X_res, columns = [testing_feature])
+    X1 = pd.DataFrame(X_res, columns = [testing_features])
 
-    train_X, test_X, train_y, test_y = train_test_split(X1, y_res, random_state=50)
+    train_X, test_X, train_y, test_y = train_test_split(X1, y_res, random_state=random.seed(time.time()))
     train_y=train_y.astype('int')
 
     vectorizer = CountVectorizer(token_pattern=r'\b\w+\b')
+    
 
-
-    train_vector = vectorizer.fit_transform(train_X[testing_feature])
-    test_vector = vectorizer.transform(test_X[testing_feature])
+    train_vector = vectorizer.fit_transform(train_X[testing_features])
+    test_vector = vectorizer.transform(test_X[testing_features])
 
     classifier.fit(train_vector, train_y.ravel())
     scores = classifier.score(test_vector, test_y)
 
-    print()
-    print( "Classifier: " + classifier_name + "\tTesting Feature: " + testing_feature + "\tIs Binary: " + str(binary_classification))
-    print( "Score: " +  ": " + str(scores))
+    result = "Classifier: " + classifier_name + "  Testing Feature: "
+    for feature in testing_features:
+        result += str(feature) 
+    result += "\tIs Binary: " + str(binary_classification)
+    result += "Score: " +  ": " + str(scores)
+    print(result)
 
     if plot_confusion_matrix:
         plotConfusionMatrix(classifier, test_vector, test_y)
